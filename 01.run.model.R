def9a9a6e161add012aadbf2853348443c2efb61
Alexey Shiklomanov <ashiklom@bu.edu>
2017-07-19 17:15:39 -0400

    Better run submission script

--- 01.run.model.R
+++ 01.run.model.R
@@ -4,86 +4,109 @@ library(tidyverse)
 
 try_data <- readRDS('traits_analysis.rds')
 
+# For testing
+if (FALSE) {
+    # For testing
+    cmdargs <- c('multi', 'area', 'clm45', 'shrub_evergreen')
+    cmdargs <- c('hier', 'mass', 'jules1')
+    cmdargs <- c('multi', 'area', 'jules1')
+}
+
 nowdate <- strftime(Sys.time(), "%Y_%m_%d")
-out_dir <- "output_fixed"
+out_dir <- "output"
 dir.create(out_dir, showWarnings = FALSE)
 
 if(!exists("cmdargs")) cmdargs <- commandArgs(trailingOnly=TRUE)
-# Possible arguments: 
-#   uni[_<pft>] -- Run univariate model for specified PFT number (no number means run all PFTs)
-#   multi[_<pft>] -- Run multivariate model for specified PFT number
-#   hier -- Run hierarchical model (global and PFT means)
-#   n.chains=n -- Run with `n` chains, where `n` is an integer.
-if(length(cmdargs) == 0){
-  cmdargs <- c("area", "uni", "uni_01", "multi", "multi_01", "hier", "n.chains=3")   # For testing
+# Arguments:
+#   1. Model type. Must be either 'multi' or 'hier'
+#   2. Data type. Must be either 'area' or 'mass'
+#   3. PFT type. Must be one of the PFT types in the TRY data.
+#   4. PFT. If 'hier', this argument must be empty. If multi and empty, fit to 
+#   all TRY data. If multi and present, fit to subset of TRY data corresponding 
+#   to provided PFT.
+stopifnot(length(cmdargs) >= 3)
+model_type <- cmdargs[1]
+stopifnot(model_type %in% c('multi', 'hier'))
+message('Selected model type: ', model_type)
+
+data_type <- cmdargs[2]
+stopifnot(data_type %in% c('area', 'mass'))
+message('Selected data type: ', data_type)
+
+pft_type <- cmdargs[3]
+valid_pft_types <- try_data %>% select_if(is.factor) %>% colnames
+stopifnot(pft_type %in% valid_pft_types)
+message('Selected PFT scheme: ', pft_type)
+
+pft <- cmdargs[4]
+if (model_type == 'hier' && !is.na(pft)) stop('Cannot specify PFT for hierarchical model')
+if (is.na(pft)) {
+    message('PFT is NA. Running for all PFTs.')
+} else {
+    valid_pfts <- try_data %>% select(one_of(pft_type)) %>% distinct %>% pull %>% levels
+    stopifnot(pft %in% valid_pfts)
+    message('Selected PFT: ', pft)
 }
-area_mass <- cmdargs[1]
-model_args <- cmdargs[-1]
-stopifnot(area_mass %in% c('area', 'mass'))
-message("Running with the following arguments:")
-print(model_args)
 
-area_rxp <- 'leaf_lifespan|LMA|area'
+area_rxp <- 'leaf_lifespan|SLA|area'
 mass_rxp <- 'leaf_lifespan|SLA|mass'
-use_rxp <- switch(area_mass, area = area_rxp, mass = mass_rxp)
+use_rxp <- switch(data_type, area = area_rxp, mass = mass_rxp)
 
-data_df <- try_data %>% 
-    dplyr::select(pft, matches(use_rxp)) %>% 
+data_df_all <- try_data %>% 
+    dplyr::select(one_of(pft_type), matches(use_rxp)) %>% 
     dplyr::filter_at(dplyr::vars(matches(use_rxp)), dplyr::any_vars(!is.na(.)))
 
-data_mat <- data_df %>% dplyr::select(-pft) %>% as.matrix() %>% log10()
-data_groups <- data_df %>% dplyr::pull(pft) %>% as.integer()
+pft_type_q <- rlang::sym(pft_type)
 
-# Get number of chains from arguments
-default.chains <- 3
-n.chains.index <- grep("n.chains", model_args)
-n.chains <- ifelse(length(n.chains.index > 0),
-                   as.numeric(gsub("n.chains=", "", model_args[n.chains.index])),
-                   default.chains)
+if (!is.na(pft)) {
+    data_df <- filter(data_df_all, UQ(pft_type_q) == pft)
+} else {
+    data_df <- data_df_all
+}
+stopifnot(nrow(data_df) > 0)
 
-# Determine which models to run from arguments
-model.args <- model_args[grep("uni|multi|hier", model_args)]
-model.args.list <- strsplit(model.args, "_")
+date_format <- strftime(Sys.time(), '%Y-%M-%d-%H%M')
+file_tag <- paste(model_type, data_type, pft_type, pft, date_format, 'rds', sep = '.')
 
-# Run models
-errors <- character()
-for (model_arg in model.args){
-    arg <- strsplit(model_arg, "_")[[1]]
-    message(paste("Running", model_arg))
-    model_type <- arg[1]
-    if (model_type %in% c('uni', 'multi')) {
-        pft_number <- as.numeric(arg[2])
-        if (!is.na(pft_number)) {
-            stopifnot(pft_number <= max(data_groups))
-            dat <- data_mat[data_groups == pft_number,]
-        } else {
-            dat <- data_mat
-        }
-        groups <- NA
-        model_name <- sprintf('%s_%s_%s', area_mass, model_type, pft_number)
-    } else if (model_type == 'hier') {
-        dat <- data_mat
-        groups <- data_groups
-        model_name <- sprintf('%s_%s', area_mass, model_type)
-    } else {
-        stop('Invalid model type "', model_type, '"')
-    }
-    out <- runModel(model_type = model_type, 
-                    dat = dat, 
-                    groups = groups, 
-                    model_name = model_name,
-                    chains = n.chains, 
-                    iter = 10000, 
-                    save_each = FALSE)
-    if(!all(is.error(out))){
-        save(out, file = sprintf("%s/%s.%s.%s.Rdata", out_dir, area_mass, model_arg, nowdate))
-    } else {
-        warning(paste("Error running", model_arg))
-        errors <- c(errors, arg)
-    }
+data_mat <- data_df %>% dplyr::select(-!!pft_type_q) %>% as.matrix() %>% log10()
+message('Data contains ', nrow(data_mat), ' rows and ', ncol(data_mat), ' columns')
+
+niter <- 300
+nchains <- 4
+parallel <- TRUE
+autofit <- TRUE
+max_attempts <- 2
+
+progress_dir <- 'progress'
+dir.create(progress_dir, showWarnings = FALSE)
+message('Saving run progress in directory: ', progress_dir)
+
+results_dir <- 'output'
+dir.create(results_dir, showWarnings = FALSE)
+message('Saving final run results in directory: ', results_dir)
+
+if (model_type == 'hier') {
+    data_groups <- data_df %>% dplyr::pull(!!pft_type_q) %>% as.integer()
+    message('Starting hierarchical model run...')
+    raw_fit <- fit_mvnorm_hier(dat = data_mat, 
+                               groups = data_groups, 
+                               niter = niter,
+                               nchains = nchains, 
+                               parallel = parallel,
+                               autofit = autofit, 
+                               max_attempts = max_attempts, 
+                               save_progress = file.path(progress_dir, file_tag))
+
+} else if (model_type == 'multi') {
+    message('Starting multivariate model run...')
+    raw_fit <- fit_mvnorm(dat = data_mat, 
+                          niter = niter,
+                          nchains = nchains, 
+                          parallel = parallel,
+                          autofit = autofit, 
+                          max_attempts = max_attempts, 
+                          save_progress = file.path(progress_dir, file_tag))
 }
 
-print("=================================")
-print("PRINT WARNINGS")
-
-warnings()
+message('Model run complete. Saving results.')
+saveRDS(raw_fit, file.path(results_dir, file_tag))
