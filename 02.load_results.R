a6903a6abe8d13ddc76454ee837e1598a392ced4
Alexey Shiklomanov (scc) <ashiklom@bu.edu>
2017-08-16 17:36:18 -0400

    Add new data loading and summarizing scripts
    Removed old ones.

---
+++ 02.load_results.R
@@ -0,0 +1,49 @@
+library(mvtraits)
+library(magrittr)
+output_dir <- "output"
+summary_dir <- "summaries"
+dir.create(summary_dir, showWarnings = FALSE)
+outputs <- list.files(output_dir)
+nfiles <- length(outputs)
+
+for (out_file in outputs) {
+    message('Processing file: ', out_file)
+    nth_file <- grep(out_file, outputs)
+    message('File ', nth_file, ' of ', nfiles)
+    
+    fname <- file.path(output_dir, out_file)
+    
+    message('Reading file...')
+    raw_output <- readRDS(fname)
+    
+    model_type <- gsub('^(multi|hier).*', '\\1', out_file)
+    is_hier <- model_type == 'hier'
+    if (is_hier) {
+        nparam <- ncol(raw_output[[1]]$mu_global)
+        ngroups <- ncol(raw_output[[1]]$mu_group) / nparam
+    } else {
+        ngroups <- NULL
+    }
+    
+    niter <- nrow(raw_output[[1]][[1]])
+    nburn <- pmax(niter - 5000, floor(0.5 * niter))
+    
+    burn <- function(samps) lapply(samps, function(x) x[nburn:niter,])
+    raw_burned <- lapply(raw_output, burn)
+    rm(raw_output); gc()
+    
+    # This step is slow!
+    message('Adding correlations...')
+    raw_withcorr <- add_correlations(raw_burned,
+                                     hier = is_hier,
+                                     ngroups = ngroups)
+    
+    message('Converting to MCMC list...')
+    raw_mcmc <- results2mcmclist(raw_withcorr, model_type)
+    rm(raw_burned, raw_withcorr); gc()
+    
+    message('Calculating summary...')
+    out_summary <- summary_df(raw_mcmc, is_hier)
+    saveRDS(out_summary, file.path(summary_dir, out_file))
+    rm(raw_mcmc); gc()
+}
---
+++ 03.load_summaries.R
@@ -0,0 +1,27 @@
+library(mvtraits)
+library(tidyverse)
+
+summaries_dir <- 'summaries'
+results_dir <- 'results'
+dir.create(results_dir, showWarnings = FALSE)
+
+summary_files <- list.files(summaries_dir)
+summary_file_pieces <- strsplit(summary_files, '\\.')
+summary_file_dat <- do.call(rbind, summary_file_pieces) %>% 
+    as_tibble() %>% 
+    select(model_type = V1, area_mass = V2, pft_scheme = V3, pft = V4) %>% 
+    mutate(fname = file.path(summaries_dir, summary_files))
+
+summary_file_full <- summary_file_dat %>% 
+    mutate(data = map(fname, readRDS))
+
+fix_groups <- function(dat) {
+    mutate(dat, group = as.character(group))
+}
+
+summary_file_fixed <- summary_file_full %>% 
+    mutate(data = map(data, fix_groups))
+
+all_summaries <- unnest(summary_file_fixed)
+
+saveRDS(all_summaries, file = file.path(results_dir, 'summaries.rds'))
