115926884f605c3f5833d6e42ae0670eb5b9e7b0
Alexey Shiklomanov <ashiklom@bu.edu>
2017-11-02 16:42:31 -0400

    Style revisions to 01.prep_submit script

--- scripts/01.prep_submit_df.R
+++ scripts/01.prep_submit_df.R
@@ -1,28 +1,32 @@
 #!/usr/bin/env Rscript
 library(nptraits)
 
-submit_tab_fname <- 'submit_df.dat'
-array_submit_fname <- 'array_submit.sh'
+submit_tab_fname <- "submit_df.dat"
+array_submit_fname <- "array_submit.sh"
 
-try_data <- readRDS('extdata/traits_analysis.rds')
+try_data <- readRDS("extdata/traits_analysis.rds")
 
 pft_classes <- try_data %>% select_if(is.factor) %>% colnames
-model_types <- c('multi', 'hier')
-data_types <- c('mass', 'area')
+model_types <- c("multi", "hier")
+data_types <- c("mass", "area")
 
 get_pfts <- function(pft_class) {
-    try_data %>% select(one_of(pft_class)) %>% pull %>% levels %>% c('', .)
+    try_data %>%
+      select(one_of(pft_class)) %>%
+      pull() %>%
+      levels %>%
+      c("", .)
 }
 
 submit_df <- expand.grid(pft_class = pft_classes, model_type = model_types, data_type = data_types,
                          stringsAsFactors = FALSE) %>%
     as_tibble() %>%
     mutate(pft = map(pft_class, get_pfts)) %>%
-    mutate(pft = case_when(.$model_type == 'hier' ~ list(''), TRUE ~ .$pft)) %>%
+    mutate(pft = case_when(.$model_type == "hier" ~ list(""), TRUE ~ .$pft)) %>%
     unnest() %>%
     select(model_type, data_type, pft_class, pft)
 
-message('Writing submission arguments to: ', submit_tab_fname)
+message("Writing submission arguments to: ", submit_tab_fname)
 write.table(submit_df, file = submit_tab_fname, quote = FALSE,
             row.names = FALSE, col.names = FALSE)
 
@@ -30,16 +34,18 @@ write.table(submit_df, file = submit_tab_fname, quote = FALSE,
 # Write qsub submission script
 ########################################
 nchains <- 4
-script <- c('#!/bin/bash -l',
-            '#$ -j y',
-            '#$ -o logs/',
+script <- c("#!/bin/bash -l",
+            "#$ -j y",
+            "#$ -o logs/",
             '#$ -q "geo*"',
-            '#$ -l mem_total=94G',
-            '#$ -l h_rt=11:59:00',
-            paste('#$ -pe omp', nchains),
-            paste0('#$ -t 1-', nrow(submit_df)),
-            'Rscript run_model.R $(head -n ${SGE_TASK_ID} submit_df.dat | tail -n 1)'
+            "#$ -l mem_total=94G",
+            "#$ -l h_rt=11:59:00",
+            paste("#$ -pe omp", nchains),
+            paste0("#$ -t 1-", nrow(submit_df)),
+            paste("Rscript run_model.R",
+                  "$(head -n ${SGE_TASK_ID} submit_df.dat ",
+                  "| tail -n 1)")
             )
-message('Writing array submission script to: ', array_submit_fname)
+message("Writing array submission script to: ", array_submit_fname)
 write(script, file = array_submit_fname)
-Sys.chmod(array_submit_fname, '0744')
+Sys.chmod(array_submit_fname, "0744")
