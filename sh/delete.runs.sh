76b34e3c7e5234239a8f84398cd5422329253a00
Alexey Shiklomanov <ashiklom@bu.edu>
2018-01-12 16:54:25 -0500

    Remove deprecated `sh` directory

--- sh/delete.runs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-while read id; do
-    qdel $id
-done < qstat.txt
--- sh/rsync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rsync -avz --progress geo:~/dietzelab/new_phytologist_traits/np-trait-analysis/results .
-#rsync -avz --progress geo:~/dietzelab/np_trait_analysis/processed_output/ processed_output/
-#rsync -avz geo:~/dietzelab/np_trait_analysis/output.n1/ output.n1/
-#rsync -avz geo:~/dl-full/ecowdery/NP_Trait_Analysis/output.n1/ output/
-#rsync -avz geo:~/dl-full/ecowdery/NP_Trait_Analysis/old.output/output/hier.trait.pft.na/ output/hier.trait.pft.na/
