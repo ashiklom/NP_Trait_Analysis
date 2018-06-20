7f9592bbceb46cc12d1712b09e7981ad3ea10d80
Alexey Shiklomanov <ashiklom@bu.edu>
2018-06-20 18:32:59 -0400

    Fix correlation tables

--- scripts/drake.R
+++ scripts/drake.R
@@ -130,9 +130,9 @@ corr_processed_plan <- drake_plan(
       significant = sign(`2.5%`) == sign(`97.5%`),
       sig_star = dplyr::if_else(significant, "*", ""),
       corr_value = sprintf("%.3f (%.3f, %.3f)%s", Mean, `2.5%`, `97.5%`, sig_star),
-      xvar = factor(xvar, names(traits_latex)) %>% forcats::lvls_revalue(unname(traits_latex)),
-      yvar = factor(yvar, names(traits_latex)) %>% forcats::lvls_revalue(unname(traits_latex)),
-      pft = forcats::fct_recode(pft, "Across-PFT" = "GLOB")
+      xvar = factor(xvar, names(traits_latex)),# %>% forcats::lvls_revalue(unname(traits_latex)),
+      yvar = factor(yvar, names(traits_latex))# %>% forcats::lvls_revalue(unname(traits_latex)),
+      ## pft = forcats::fct_recode(pft, "Across-PFT" = "GLOB")
     ) %>%
     ## dplyr::select(pft, yvar, xvar, corr_value) %>%
     dplyr::arrange(pft)
@@ -160,6 +160,28 @@ corr_plot <- function(data, ma) {
     labs(color = "PFT")
 }
 
+corr_grid <- function(data) {
+  data %>%
+    mutate(
+      significant = factor(significant, c("TRUE", "FALSE")) %>%
+        forcats::lvls_revalue(c("yes", "no"))
+    ) %>%
+  ggplot() +
+    aes(x = present + 0.1, y = Mean ^ 2, color = pft) + # size = significant
+    geom_point() +
+    geom_smooth(aes(group = 1), method = "loess", color = NA, span = 2) +
+    scale_x_log10(breaks = c(1, 100, 10000), minor_breaks = c(10, 1000), limits = c(0.1, 30000)) +
+    coord_cartesian(ylim = c(0, 1)) +
+    ## scale_size_manual(values = c("yes" = 3, "no" = 1)) +
+    scale_color_manual(name = "PFT", values = c("black", pft_colors)) +
+    theme_bw()
+}
+
+if (FALSE) {
+
+
+}
+
 text_plot <- function(text) {
   ggplot() +
     geom_text(aes(label = text, x = 0, y = 0)) +
@@ -172,6 +194,71 @@ text_plot <- function(text) {
 }
 
 range_plan <- drake_plan(
+  mass_area_grid = c(
+       1, 1, 1, 1, 1, 1,
+    2,    1, 1, 1, 1, 1,
+    2, 2,    1, 1, 1, 1,
+    2, 2, 2,    1, 1, 1,
+    2, 2, 2, 2,    1, 1,
+    2, 2, 2, 2, 2,    1,
+    2, 2, 2, 2, 2, 2
+  ),
+  yvar_grid = c(
+       1, 1, 1, 1, 1, 1,
+    1,    2, 2, 2, 2, 2,
+    1, 2,    3, 3, 3, 3,
+    1, 2, 3,    4, 4, 4,
+    1, 2, 3, 4,    5, 5,
+    1, 2, 3, 4, 5,    6,
+    1, 2, 3, 4, 5, 6
+  ),
+  xvar_grid = c(
+       2, 3, 4, 5, 6, 7,
+    2,    3, 4, 5, 6, 7,
+    3, 3,    4, 5, 6, 7,
+    4, 4, 4,    5, 6, 7,
+    5, 5, 5, 5,    6, 7,
+    6, 6, 6, 6, 6,    7,
+    7, 7, 7, 7, 7, 7
+  ),
+  trait_names = c("leaf_lifespan", "SLA", "N", "P", "Rd", "Vcmax", "Jmax"),
+  trait_grid_table = tibble::tibble(
+    mass_area = mass_area_grid,
+    xvar = xvar_grid,
+    yvar = yvar_grid
+  ) %>%
+    dplyr::mutate(
+      mass_area = factor(mass_area) %>% forcats::lvls_revalue(c("mass", "area")),
+      xvar = factor(xvar, 1:7) %>% forcats::lvls_revalue(trait_names),
+      yvar = factor(yvar, 1:7) %>% forcats::lvls_revalue(trait_names),
+    ),
+  plt_df_orig = corr_processed %>%
+    dplyr::group_by(mass_area, xvar, yvar) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(
+      plt = purrr::map(data, corr_grid),
+      xvar = stringr::str_remove(as.character(xvar), "_?(mass|area)") %>% factor(trait_names),
+      yvar = stringr::str_remove(as.character(yvar), "_?(mass|area)") %>% factor(trait_names)
+    ) %>%
+    dplyr::ungroup(),
+  plt_df = dplyr::left_join(trait_grid_table, plt_df_orig),
+  plt_list = plt_df$plt %>%
+    append(list(text_plot("Leaf lifespan")), 0) %>%
+    append(list(text_plot("SLA")), 8) %>%
+    append(list(text_plot("N")), 16) %>%
+    append(list(text_plot("P")), 24) %>%
+    append(list(text_plot("Rd")), 32) %>%
+    append(list(text_plot("Vcmax")), 40) %>%
+    append(list(text_plot("Jmax")), 48),
+  corr_ss_plot_gg = GGally::ggmatrix(
+    plt_list,
+    nrow = 7,
+    ncol = 7,
+    xlab = "Sample size",
+    ylab = "Squared pairwise correlation",
+    legend = 2,
+    progress = FALSE
+  ),
   try_long = try_dat %>% tidyr::gather(trait, value, -pft, na.rm = TRUE),
   try_ranges = try_long %>%
     dplyr::group_by(trait) %>%
@@ -223,10 +310,106 @@ range_plan <- drake_plan(
   )
 )
 
+censor <- function(rxp, pft, pfts, param, value) {
+  if_else(grepl(rxp, param) & pfts %in% pft, "---", value)
+}
+
+## param_tex <- param_markdown
+## ## stringr::str_replace(param_markdown, " \\(", "\\\\(")
+## names(param_tex) <- names(param_markdown)
+
 results_plan <- drake_plan(
   results_all = readRDS(file_in("results/mvtraits_results.rds")),
-  
-  )
+  multi_summary = results_all %>%
+    dplyr::filter(model_type %in% c("uni", "multi")) %>%
+    dplyr::mutate(
+      mu_mean = map(data, c("result", "mu", "Mean")) %>%
+        purrr::map(~tibble::as_tibble(as.list(.))),
+      mu_lo = map(data, c("result", "mu", "2.5%")) %>%
+        purrr::map(~tibble::as_tibble(as.list(.))),
+      mu_hi = map(data, c("result", "mu", "97.5%")) %>%
+        purrr::map(~tibble::as_tibble(as.list(.)))
+    ) %>%
+    dplyr::select(model_type:pft, mu_mean:mu_hi) %>%
+    tidyr::unnest(.sep = ".") %>%
+    tidyr::gather("variable", "value", -(model_type:pft), na.rm = TRUE) %>%
+    tidyr::separate(variable, c("stat", "param"), sep = "\\.") %>%
+    tidyr::spread(stat, value) %>%
+    dplyr::mutate(
+      pft = factor(pft, abbr2pft) %>% forcats::lvls_revalue(pft2abbr),
+      param = factor(param, both_params)
+    ),
+  hier_prep = results_all %>%
+    dplyr::filter(model_type == "hier") %>%
+    dplyr::select(model_type:pft_type, data) %>%
+    dplyr::mutate(
+      mu_vals = purrr::map(data, c("result", "mu_group")),
+      pft = purrr::map(mu_vals, names),
+      mu_df = purrr::map2(pft, mu_vals, ~tibble::tibble(pft = ..1, dat = ..2))
+    ) %>%
+    dplyr::select(-data, -mu_vals, -pft) %>%
+    tidyr::unnest(),
+  hier_summary = hier_prep %>%
+    dplyr::mutate(
+      mu_mean = purrr::map(dat, c("Mean")) %>%
+        purrr::map(~tibble::as_tibble(as.list(.))),
+      mu_lo = purrr::map(dat, c("2.5%")) %>%
+        purrr::map(~tibble::as_tibble(as.list(.))),
+      mu_hi = purrr::map(dat, c("97.5%")) %>%
+        purrr::map(~tibble::as_tibble(as.list(.)))
+    ) %>%
+    dplyr::select(model_type:pft, mu_mean:mu_hi) %>%
+    tidyr::unnest(.sep = ".") %>%
+    tidyr::gather("variable", "value", -(model_type:pft), na.rm = TRUE) %>%
+    tidyr::separate(variable, c("stat", "param"), sep = "\\.") %>%
+    tidyr::spread(stat, value) %>%
+    dplyr::mutate(
+      pft = dplyr::if_else(is.na(pft), "global", pft),
+      pft = factor(pft, abbr2pft) %>% forcats::lvls_revalue(pft2abbr),
+      param = factor(param, both_params)
+    ),
+  # Absolute comparison of means
+  means_dat = multi_summary %>%
+    dplyr::filter(pft != "GLOB", !is.na(pft)) %>%
+    dplyr::bind_rows(hier_summary) %>%
+    dplyr::mutate(
+      model_type = factor(model_type, c("uni", "multi", "hier")) %>%
+        forcats::lvls_revalue(c("univariate", "multivariate", "hierarchical")),
+      mass_area = factor(mass_area, c("mass", "area"))
+    ) %>%
+    dplyr::mutate_at(
+      c("mu_mean", "mu_lo", "mu_hi"),
+      ~10 ^ .
+    ) %>%
+    dplyr::filter(
+      !(param %in% c("leaf_lifespan", "SLA") & mass_area == "area")
+    ),
+  table_dat = means_dat %>%
+    dplyr::filter(
+      model_type == "hierarchical",
+      !(param %in% c("SLA", "leaf_lifespan") & mass_area == "area")
+    ) %>%
+    dplyr::mutate(
+      mu_lo = dplyr::if_else(param %in% c("Nmass", "Pmass"), mu_lo * 100, mu_lo),
+      mu_hi = dplyr::if_else(param %in% c("Nmass", "Pmass"), mu_hi * 100, mu_hi),
+      mu_mean = dplyr::if_else(param %in% c("Nmass", "Pmass"), mu_mean * 100, mu_mean),
+      lo_f = formatC(mu_lo, digits = 3),
+      mid_f = formatC(mu_mean, digits = 3),
+      hi_f = formatC(mu_hi, digits = 3),
+      value = sprintf("%s (%s,%s)", mid_f, lo_f, hi_f),
+      value = censor("Vcmax|Jmax", "C4G", pft, param, value),
+      value = censor("Rdmass", "ShDBo", pft, param, value),
+      value = censor("Jmax_mass", c("NlEBo", "NlD"), pft, param, value),
+      value = censor("Jmax_area", c("NlD"), pft, param, value)
+    ) %>%
+    dplyr::select(pft, param, value) %>%
+    dplyr::mutate(
+      param = factor(param, both_params) %>% 
+        forcats::lvls_revalue(param_markdown_nounit[both_params]),
+      ) %>%
+    tidyr::spread(param, value) %>%
+    dplyr::arrange(pft)
+)
 
 plan <- rbind(
   base_plan,
@@ -248,6 +431,12 @@ if (FALSE) {
   readd(corr_plots)$plt[[1]]
 
   clean(corr_plots)
+
+  readd(sample_size_long)
+
+  corr_results <- readd(corr_results)
+ 
+  plan$target
 
   readd(corr_range_plot_gg)
 }
