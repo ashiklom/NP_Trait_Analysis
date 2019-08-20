f34c2e9c5d83210edda8c762dcfed7fd681c6c6c
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-08-20 09:23:39 -0400

    Update dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
 ;; Use drake for compilation
-((nil . ((compile-command . "Rscript scripts/drake.R"))))
+((ess-r-mode . ((ess-r-package-dirs . (("R" . 1) ("tests" . 1) ("testthat" . 2)
+                                       ("scripts" . 1) ("manuscript-analyses" . 2) ("presentations" . 2)))))
+ (nil . ((compile-command . "Rscript scripts/drake.R"))))
