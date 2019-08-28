8a586f84543837407653c7457101fd7826a41c6e
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-08-28 17:59:44 -0400

    Closer draft of n-fold validation code

--- scripts/n-fold-fake.R
+++ scripts/n-fold-fake.R
@@ -15,51 +15,57 @@ data_mat <- dat_df_all %>%
   select(-pft) %>%
   as.matrix()
 
-r_hm <- readRDS("output/hier.mass.clm45.NA.2018-02-12-1154.rds")
+r_hm <- readRDS("output/hier.mass.clm45.NA.2019-08-26-1533.rds")
 
-# Create a matrix of indices (which I will eventually sample)
+# Remove unnecessary stuff from the hierarchical output so that
+r_hm_mat <- coda:::as.matrix.mcmc.list(
+  window(r_hm$samples, start = 10000)
+)
+r_hm_mat <- r_hm_mat[, !grepl("^Corr", colnames(r_hm_mat))]
+r_hm_mat <- r_hm_mat[, !grepl("global", colnames(r_hm_mat))]
+
+# Create a matrix of indices (which I will eventually sample) where at least 2
+# traits are present
 present <- !is.na(data_mat)
-ind_mat <- matrix(0, nrow(data_mat), ncol(data_mat))
-ind_mat[present] <- seq_len(sum(present))
+cv_rows <- matrix(rowSums(present) >= 2, nrow(data_mat), ncol(data_mat))
+sample_inds <- which(cv_rows & present)
 
-# Subset to rows where at least 2 traits are present
-cv_rows <- rowSums(!is.na(data_mat)) >= 2
-ind_mat_use <- ind_mat[cv_rows, ]
-sample_inds <- ind_mat_use[ind_mat_use != 0]
+# Rows: ((i - 1) %% nrow) + 1
+# Cols: ((i - 1) %/% nrow) + 1
 
 ncv <- 5
 nremove <- 1000
 
-library(mvtraits)
-
-l <- list()
-
 library(foreach, exclude = c("accumulate", "when"))
 library(doFuture)
-
-options(future.globals.maxSize = 700 * 1024^2)
 
 future::plan(future.callr::callr())
 registerDoFuture()
 
 l <- times(ncv) %dopar% {
+## for (i in seq_len(ncv)) {
   # create missing data
   imissing <- sort(sample(sample_inds, nremove))
+  imissing_m <- cbind(
+    i = imissing,
+    row = ((imissing - 1) %% nrow(data_mat)) + 1,
+    col = ((imissing - 1) %/% nrow(data_mat)) + 1
+  )
   true_vals <- data_mat[imissing]
   missing_dat <- data_mat
   missing_dat[imissing] <- NA
-  mmissing <- which(is.na(missing_dat), arr.ind = TRUE)
+  ## mmissing <- which(is.na(missing_dat), arr.ind = TRUE)
   # Bootstrap missing data
-  boot <- bootstrap_missing_hier(r_hm, missing_dat, data_groups, n = 20)
+  boot <- mvtraits::bootstrap_missing_hier(r_hm_mat, missing_dat, data_groups)
   # Summarize more efficiently
-  boot_out <- matrix(numeric(), nrow(mmissing), dim(boot)[[3]])
+  boot_out <- matrix(numeric(), length(imissing), dim(boot)[[3]])
   for (j in seq_len(ncol(boot_out))) {
-    boot_out[,j] <- boot[,,j][mmissing]
+    boot_out[,j] <- boot[,,j][imissing]
   }
   boot_means <- rowMeans(boot_out)
-  list(true = true_vals, boot = boot_means)
+  list(true = true_vals, boot = boot_means, inds = imissing_m)
 }
 
-for (i in seq_len(ncv)) {
-  message("Iteration ", i, " of ", ncv)
-}
+# Something's not right here...
+jj <- imissing_m[, "col"] == 4
+plot(true_vals[jj] ~ boot_means[jj])
