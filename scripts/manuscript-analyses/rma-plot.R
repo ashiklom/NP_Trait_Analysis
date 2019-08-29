e429102d3946f1f5244e9749ce90e89f16695e68
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-08-29 16:23:13 -0400

    Add RMA tile plot figure

--- scripts/manuscript-analyses/rma-plot.R
+++ scripts/manuscript-analyses/rma-plot.R
@@ -6,9 +6,6 @@ hier_area_file <- tail(dir(outdir, "hier.area.clm45", full.names = TRUE), 1)
 multi_mass_file <- tail(dir(outdir, "multi.mass.clm45.NA", full.names = TRUE), 1)
 multi_area_file <- tail(dir(outdir, "multi.area.clm45.NA", full.names = TRUE), 1)
 
-hier_mass <- readRDS(hier_mass_file)
-samples <- hier_mass$samples[[1]]
-
 rma_slope <- function(m, pft, yvar, xvar) {
   sdy_col <- paste("Sigma", pft, yvar, yvar, sep = "..")
   sdx_col <- paste("Sigma", pft, xvar, xvar, sep = "..")
@@ -37,28 +34,95 @@ all_rma_slopes <- function(samples) {
   out
 }
 
-tidy_rma <- function(output) {
-  raw_result <- lapply(hier_mass$samples, all_rma_slopes) %>%
+tidy_rma <- function(output, mass_area) {
+  raw_result <- lapply(output$samples, all_rma_slopes) %>%
     do.call(what = rbind)
-
   raw_means <- colMeans(raw_result)
+  rma_q <- t(apply(raw_result, 2, quantile, c(0.025, 0.975))) %>%
+    tibble::as_tibble(rownames = "colname") %>%
+    dplyr::rename(lo = `2.5%`, hi = `97.5%`)
+  rma_df <- tibble::enframe(raw_means, "colname", "Mean") %>%
+    dplyr::left_join(rma_q, "colname")
+  param <- switch(
+    mass_area,
+    mass = shiklomanov2017np::mass_params,
+    area = shiklomanov2017np::area_params
+  )
+  rma_split <- rma_df %>%
+    tidyr::separate(colname, c("stat", "pft", "xvar", "yvar"),
+                    sep = "\\.\\.") %>%
+    dplyr::mutate(
+      mass_area = !!mass_area,
+      yvar = factor(yvar, param),
+      xvar = factor(xvar, param),
+      pft = forcats::fct_inorder(pft)
+    )
+  rma_split
 }
 
-rma_means <- colMeans(rma_result)
-rma_q <- t(apply(rma_result, 2, quantile, c(0.025, 0.975))) %>%
-  tibble::as_tibble(rownames = "colname") %>%
-  dplyr::rename(lo = `2.5%`, hi = `97.5%`)
-rma_df <- tibble::enframe(rma_means, "colname", "Mean") %>%
-  dplyr::left_join(rma_q, "colname")
-
-rma_split <- rma_df %>%
-  tidyr::separate(colname, c("stat", "pft", "xvar", "yvar"),
-                  sep = "\\.\\.") %>%
-  dplyr::mutate(
-    yvar = factor(yvar, mass_params),
-    xvar = factor(xvar, mass_params),
-    pft = forcats::fct_inorder(pft)
+rma_mass <- readRDS(hier_mass_file) %>% tidy_rma("mass")
+rma_area <- readRDS(hier_area_file) %>% tidy_rma("area")
+rma_both <- bind_rows(rma_mass, rma_area) %>%
+  mutate(
+    xvar2 = stringr::str_remove(xvar, "_?(mass|area)") %>%
+      forcats::fct_inorder(),
+    yvar2 = stringr::str_remove(yvar, "_?(mass|area)") %>%
+      forcats::fct_inorder(),
+    pft = forcats::fct_recode(pft, !!!abbr2pft),
+    significant = sign(lo) == sign(hi),
+    sigstar = if_else(significant, "*", NA_character_)
   )
+
+plt <- ggplot(rma_both) +
+  aes(x = pft, y = "", fill = Mean, label = sigstar) +
+  geom_tile() +
+  geom_text(size = 6) +
+  facet_nested(yvar2 + xvar2 ~ mass_area, switch = "y") +
+  labs(
+    x = "Plant functional type",
+    y = "Trait pair",
+    fill = "RMA Slope"
+  ) +
+  scale_fill_gradient2(low = "red4", mid = "white", high = "blue4") +
+  cowplot::theme_cowplot() +
+  theme(
+    axis.text.x = element_text(angle = 90),
+    panel.spacing.y = unit(0.05, "lines"),
+    strip.placement = "outside",
+    strip.text.y = element_text(angle = 180)
+  )
+
+ggsave("figures/manuscript/rma-slope-tiles.png", plt,
+       width = 13.5, height = 7.9)
+
+rma_split %>%
+  mutate(significant = sign(lo) == sign(hi),
+         sigstar = if_else(significant, "*", NA_character_),
+         expected = if_else(yvar == "leaf_lifespan", -1, 1),
+         positive = Mean > 0,
+         les = sign(Mean) == sign(expected),
+         pft = fct_recode(pft, !!!abbr2pft)) %>%
+  ggplot() +
+  ## aes(x = pft, y = interaction(xvar, yvar, sep = "-"), fill = Mean,
+  ##     label = sigstar) +
+
+rma_barplot <- function(data, xvar, yvar) {
+  data %>%
+    filter(xvar == !!xvar, yvar == !!yvar) %>%
+    ggplot() +
+    aes(x = pft, y = Mean, ymin = lo, ymax = hi, fill = pft) +
+    geom_col() +
+    geom_errorbar() +
+    scale_fill_manual(values = c("gray25", pft_colors)) +
+    guides(fill = FALSE) +
+    theme_minimal() +
+    theme(
+      axis.title = element_blank(),
+      axis.text = element_blank()
+    )
+}
+
+rma_barplot(rma_split, "SLA", "leaf_lifespan")
 
 rma_split %>%
   ## filter(sign(lo) == sign(hi)) %>%
@@ -66,8 +130,9 @@ rma_split %>%
   aes(x = pft, y = Mean, ymin = lo, ymax = hi, fill = pft) +
   geom_col() +
   geom_errorbar() +
-  facet_grid(vars(yvar), vars(xvar), scales = "free_y") +
+  facet_grid(vars(yvar), vars(xvar), scales = "free_y", drop = FALSE) +
   scale_fill_manual(values = c("gray25", pft_colors)) +
+  theme_bw() +
   theme(axis.text.x = element_blank(),
         legend.position = "bottom")
 
