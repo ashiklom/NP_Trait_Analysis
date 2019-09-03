2785332ce621e10a499f9bbc5fbefbdf9852debf
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-09-03 14:56:49 -0400

    Comment out stickplot fig in many_results script

--- scripts/manuscript-analyses/many_results.R
+++ scripts/manuscript-analyses/many_results.R
@@ -385,38 +385,38 @@ ggsave(file.path(manuscript_fig_dir, "relative_ci_model.pdf"),
        width = 4.4, height = 3)
 
 ## ----stickpairs, fig.width = 5, fig.height = 5, fig.cap = '(ref:stickcap)'----
-results_sub <- results_all %>%
-  filter((model_type == "multi" & pft == "global") |
-         (model_type == "hier"))
-dat_list <- map(results_sub$data, "result")
-names(dat_list) <- with(results_sub, paste(model_type, mass_area, sep = "_"))
+## results_sub <- results_all %>%
+##   filter((model_type == "multi" & pft == "global") |
+##          (model_type == "hier"))
+## dat_list <- map(results_sub$data, "result")
+## names(dat_list) <- with(results_sub, paste(model_type, mass_area, sep = "_"))
 
-if (!interactive()) pdf(file.path(manuscript_fig_dir, "stick_pairs.pdf"),
-                        width = 5, height = 5)
-stickplot_pairs(
-  mu_global_lower = dat_list$multi_mass$mu,
-  Sigma_global_lower = dat_list$multi_mass$Sigma,
-  mu_group_lower = dat_list$hier_mass$mu_group,
-  Sigma_group_lower = dat_list$hier_mass$Sigma_group,
-  vars_lower = mass_params,
-  group_names = pft2abbr[-1],
-  vars_label = param_labels,
-  unlog_axes = TRUE,
-  mu_global_upper = dat_list$multi_area$mu,
-  Sigma_global_upper = dat_list$multi_area$Sigma,
-  mu_group_upper = dat_list$hier_area$mu_group,
-  Sigma_group_upper = dat_list$hier_area$Sigma_group,
-  vars_upper = area_params,
-  col_group = pft_colors,
-  cex_g = 1.2,
-  lwd_g = 1,
-  lty_g = 1,
-  lwd_s = 1.3,
-  par_plot = list(oma = c(0, 5, 3, 5), cex = 0.4),
-  par_legplot = list(mar = c(0, 0, 0, 0)),
-  par_legend = list(ncol = 5, x = "center", cex = 0.6),
-  reorder_legend = TRUE
-)
-mtext("Mass-normalized", side = 2, cex = 0.7, line = 3)
-mtext("Area-normalized", side = 4, cex = 0.7, line = 1)
-if (!interactive()) dev.off()
+## if (!interactive()) pdf(file.path(manuscript_fig_dir, "stick_pairs.pdf"),
+##                         width = 5, height = 5)
+## stickplot_pairs(
+##   mu_global_lower = dat_list$multi_mass$mu,
+##   Sigma_global_lower = dat_list$multi_mass$Sigma,
+##   mu_group_lower = dat_list$hier_mass$mu_group,
+##   Sigma_group_lower = dat_list$hier_mass$Sigma_group,
+##   vars_lower = mass_params,
+##   group_names = pft2abbr[-1],
+##   vars_label = param_labels,
+##   unlog_axes = TRUE,
+##   mu_global_upper = dat_list$multi_area$mu,
+##   Sigma_global_upper = dat_list$multi_area$Sigma,
+##   mu_group_upper = dat_list$hier_area$mu_group,
+##   Sigma_group_upper = dat_list$hier_area$Sigma_group,
+##   vars_upper = area_params,
+##   col_group = pft_colors,
+##   cex_g = 1.2,
+##   lwd_g = 1,
+##   lty_g = 1,
+##   lwd_s = 1.3,
+##   par_plot = list(oma = c(0, 5, 3, 5), cex = 0.4),
+##   par_legplot = list(mar = c(0, 0, 0, 0)),
+##   par_legend = list(ncol = 5, x = "center", cex = 0.6),
+##   reorder_legend = TRUE
+## )
+## mtext("Mass-normalized", side = 2, cex = 0.7, line = 3)
+## mtext("Area-normalized", side = 4, cex = 0.7, line = 1)
+## if (!interactive()) dev.off()
